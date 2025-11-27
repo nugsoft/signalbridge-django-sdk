40f85364c21e120d5110a22ddee22275921bed4b
William Asaba <asabawilliamdk@yahoo.com>
2025-11-27 12:28:29 +0300

    chore: initialize signalbridge package
    Added __init__.py to make signalbridge a proper Python package.

---
+++ signalbridge/__init__.py
@@ -0,0 +1,14 @@
+"""
+SignalBridge Django SDK
+~~~~~~~~~~~~~~~~~~~~~~~
+
+Django SDK for SignalBridge SMS Gateway.
+
+:copyright: (c) 2025 by Nugsoft.
+:license: MIT, see LICENSE for more details.
+"""
+
+__version__ = '1.0.0'
+__author__ = 'Nugsoft'
+
+default_app_config = 'signalbridge.apps.SignalBridgeConfig'
